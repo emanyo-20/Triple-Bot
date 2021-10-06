94c85a508df9d835a44e3a971936b01861b5a4a6
emanyo-20 <85400817+emanyo-20@users.noreply.github.com>
2021-10-07 02:56:35 +0300

    Update eg.py

--- eg.py
+++ eg.py
@@ -267,7 +267,7 @@ PHONENUMBER: {str(phone)}\n
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
 # saving will hapen after delay 2 seconds.
-bot.enable_save_next_step_handlers(delay=0)
+bot.enable_save_next_step_handlers(delay=1)
 
 # Load next_step_handlers from save file (default "./.handlers-saves/step.save")
 # WARNING It will work only if enable_save_next_step_handlers was called!
